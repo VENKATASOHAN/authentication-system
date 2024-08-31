8af8eab575b8b56ae06d0db09454280748e46b4c
Avula Venkata Sohan <101496156+VENKATASOHAN@users.noreply.github.com>
2024-09-01 00:50:53 +0530

    Update protected.js

--- auth-system/routes/protected.js
+++ auth-system/routes/protected.js
@@ -1,1 +1,10 @@
-e
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+
+router.get('/', auth, (req, res) => {
+    res.send('Protected route accessed');
+});
+
+module.exports = router;
+
